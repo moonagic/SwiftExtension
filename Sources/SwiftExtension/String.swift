29def6a26996d6bcae91216cb31e87e3d9588878
moonagic <i@moonagic.com>
2020-11-23 15:15:57 +0800

    Add UIViewController extension

--- Sources/SwiftExtension/String.swift
+++ Sources/SwiftExtension/String.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CommonCrypto
 
 extension String {
     
---
+++ Sources/SwiftExtension/UIViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController.swift
+//  SwiftExtension
+//
+//  Created by Moon on 11/23/20.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    public var isModal: Bool {
+        
+        let presentingIsModal = presentingViewController != nil
+        let presentingIsNavigation = navigationController?.presentingViewController?.presentedViewController == navigationController
+        let presentingIsTabBar = tabBarController?.presentingViewController is UITabBarController
+        
+        return presentingIsModal || presentingIsNavigation || presentingIsTabBar
+    }
+}
