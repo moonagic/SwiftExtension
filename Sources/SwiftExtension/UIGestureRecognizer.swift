17149c7fdefb6e76ea811e813ce4b89683e5b853
moonagic <i@moonagic.com>
2020-12-18 14:39:03 +0800

    Update

--- Sources/SwiftExtension/UIGestureRecognizer.swift
+++ Sources/SwiftExtension/UIGestureRecognizer.swift
@@ -10,25 +10,25 @@ import UIKit
 
 
 extension UIGestureRecognizer {
-    typealias MGGestureBlock = ((Any)->())
+    public typealias MGGestureBlock = ((Any)->())
     
-    fileprivate struct RuntimeKey {
+    public struct RuntimeKey {
         static let GestureBlockKey = UnsafeRawPointer.init(bitPattern: "GestureBlockKey".hashValue)
     }
     
-    convenience init(actionBlock: @escaping MGGestureBlock) {
+    public convenience init(actionBlock: @escaping MGGestureBlock) {
         self.init()
         addActionBlock(actionBlock)
         addTarget(self, action: #selector(invoke(_:)))
     }
     
-    fileprivate func addActionBlock(_ block: MGGestureBlock?) {
+    public func addActionBlock(_ block: MGGestureBlock?) {
         if (block != nil) {
             objc_setAssociatedObject(self, UIGestureRecognizer.RuntimeKey.GestureBlockKey!, block!, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
     
-    @objc fileprivate func invoke(_ sender: Any) {
+    @objc public func invoke(_ sender: Any) {
         let block = objc_getAssociatedObject(self, UIGestureRecognizer.RuntimeKey.GestureBlockKey!) as? MGGestureBlock
         if (block != nil) {
             block!(sender);
