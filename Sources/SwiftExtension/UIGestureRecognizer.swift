2d42b1c253808203eacb340b716fb5621a9f8d13
moonagic <i@moonagic.com>
2020-12-18 14:22:38 +0800

    Update

--- Sources/SwiftExtension/UIGestureRecognizer.swift
+++ Sources/SwiftExtension/UIGestureRecognizer.swift
@@ -8,72 +8,30 @@
 import Foundation
 import UIKit
 
-extension UIGestureRecognizer {
-    @discardableResult public convenience init(addToView targetView: UIView, closure: @escaping () -> Void) {
-        self.init()
-        
-        GestureTarget.add(gesture: self, closure: closure, toView: targetView)
-    }
-}
 
-fileprivate class GestureTarget: UIView {
-    class ClosureContainer {
-        weak var gesture: UIGestureRecognizer?
-        let closure: (() -> Void)
-        
-        init(closure: @escaping () -> Void) {
-            self.closure = closure
+extension UIGestureRecognizer {
+    typealias MGGestureBlock = ((Any)->())
+    
+    fileprivate struct RuntimeKey {
+        static let GestureBlockKey = UnsafeRawPointer.init(bitPattern: "GestureBlockKey".hashValue)
+    }
+    
+    convenience init(actionBlock: @escaping MGGestureBlock) {
+        self.init()
+        addActionBlock(actionBlock)
+        addTarget(self, action: #selector(invoke(_:)))
+    }
+    
+    fileprivate func addActionBlock(_ block: MGGestureBlock?) {
+        if (block != nil) {
+            objc_setAssociatedObject(self, UIGestureRecognizer.RuntimeKey.GestureBlockKey!, block!, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
     
-    var containers = [ClosureContainer]()
-    
-    convenience init() {
-        self.init(frame: .zero)
-        isHidden = true
-    }
-    
-    class func add(gesture: UIGestureRecognizer, closure: @escaping () -> Void,
-                   toView targetView: UIView) {
-        let target: GestureTarget
-        if let existingTarget = existingTarget(inTargetView: targetView) {
-            target = existingTarget
-        } else {
-            target = GestureTarget()
-            targetView.addSubview(target)
-        }
-        let container = ClosureContainer(closure: closure)
-        container.gesture = gesture
-        target.containers.append(container)
-        
-        gesture.addTarget(target, action: #selector(GestureTarget.target(gesture:)))
-        targetView.addGestureRecognizer(gesture)
-    }
-    
-    class func existingTarget(inTargetView targetView: UIView) -> GestureTarget? {
-        for subview in targetView.subviews {
-            if let target = subview as? GestureTarget {
-                return target
-            }
-        }
-        return nil
-    }
-    
-    func cleanUpContainers() {
-        containers = containers.filter({ $0.gesture != nil })
-    }
-    
-    @objc func target(gesture: UIGestureRecognizer) {
-        cleanUpContainers()
-        
-        for container in containers {
-            guard let containerGesture = container.gesture else {
-                continue
-            }
-            
-            if gesture === containerGesture {
-                container.closure()
-            }
+    @objc fileprivate func invoke(_ sender: Any) {
+        let block = objc_getAssociatedObject(self, UIGestureRecognizer.RuntimeKey.GestureBlockKey!) as? MGGestureBlock
+        if (block != nil) {
+            block!(sender);
         }
     }
 }
