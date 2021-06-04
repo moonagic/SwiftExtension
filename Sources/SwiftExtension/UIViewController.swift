969e91b9ff189978a403702118167d47a340a3ce
moonagic <i@moonagic.com>
2021-06-04 17:55:14 +0800

    添加函数.

--- Sources/SwiftExtension/UIViewController.swift
+++ Sources/SwiftExtension/UIViewController.swift
@@ -34,4 +34,23 @@ extension UIViewController {
         return self.presentedViewController!.topMostViewController()
     }
     
+    public class func topViewController(_ viewController: UIViewController? = nil) -> UIViewController? {
+        let viewController = viewController ?? UIApplication.shared.keyWindow?.rootViewController
+
+        if let navigationController = viewController as? UINavigationController,
+            !navigationController.viewControllers.isEmpty {
+            return self.topViewController(navigationController.viewControllers.last)
+
+        } else if let tabBarController = viewController as? UITabBarController,
+            let selectedController = tabBarController.selectedViewController {
+            return self.topViewController(selectedController)
+
+        } else if let presentedController = viewController?.presentedViewController {
+            return self.topViewController(presentedController)
+            
+        }
+
+        return viewController
+    }
+    
 }
